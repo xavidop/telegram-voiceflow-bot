60c2fe106ce8ccd065040972d6e26f262162817e
Xavier Portilla Edo <xavi_tb@hotmail.com>
2021-01-30 23:45:45 +0100

    telegram bot using voiceflow

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const { Telegraf } = require('telegraf')
 const { App } = require('@voiceflow/runtime-client-js');
+require('dotenv').config();
+
 let chatbot;
+let conversationEnded = false;
 
-
-const bot = new Telegraf('1684324609:AAEonQL1LQfEoRu3w_uUqPXejQ2yZNwIn6Q');
-
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 bot.start(async (ctx) => {
-  chatbot = new App({
-    versionID: '600b11fb8bf190001bafc381',
-    endpoint: 'https://localhost:4000'
-  });
-
-  const newState = await chatbot.start();
-
-  let newstate2 = await chatbot.sendText("hi");
-  ctx.reply("hola");	
+  conversationEnded = false;
+  let startState = await initializeclient(true);
+  ctx.reply(startState.trace[0].payload.message);	
 });
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+const regex = new RegExp(/(.+)/i)
+bot.hears(regex, async (ctx) =>{ 
+    let replay = '';
+    if(!conversationEnded){
+        await initializeclient();
+        const newState = await chatbot.sendText(ctx.message.text);
+        
+        if(newState.trace.length === 0){
+            replay += "Sorry, I did not understand you. Can you repeat, please?"
+        }else{
+            replay = newState.trace[0].payload.message;
+        }
+
+        if(newState.end){
+            replay += "\nIf you want to start again just write /start"
+            conversationEnded = true;
+        }
+    }else{
+        replay = "\nThe Conversation has ended. If you want to start again just write /start"
+    }
+
+    ctx.reply(replay)
+})
 
 bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
+async function initializeclient(forceRestart){
+    if(chatbot === null || chatbot === undefined || forceRestart){
+        chatbot = new App({
+            versionID: process.env.VOICEFLOW_PROGRAM
+        });
+        return startState = await chatbot.start();
+    }
+    return null;
+}
